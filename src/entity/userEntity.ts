bdb3f481e596a4816b36eca66fa62330111caa67
4shaham <143824921+4shaham@users.noreply.github.com>
2024-06-05 11:32:16 +0530

    userSchema created

---
+++ src/entity/userEntity.ts
@@ -0,0 +1,14 @@
+
+
+export default interface IUser {
+
+    _id: string;
+    email: string | null;
+    userName: string | null;
+    phoneNumber: number | null;
+    password: string | null;
+    age: number | null;
+    gender: string | null;
+    status: boolean | null;
+
+  }
---
+++ src/framework/model/UserSchema.ts
@@ -0,0 +1,52 @@
+
+import mongoose, {Schema,Model } from "mongoose";
+import  IUser  from "../../entity/userEntity";
+
+
+const UserSchema:Schema=new Schema({
+
+    userName:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:Boolean,
+        default:false
+    }
+},
+{
+    timestamps: true 
+})
+
+
+const Users=mongoose.model<IUser>('Users',UserSchema)
+
+export default Users
+
+
+
+
+
+
+  
